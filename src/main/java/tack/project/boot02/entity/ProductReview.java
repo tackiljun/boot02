4d2c2c9637550abf023fdd74213203980e4ed9fb
tarkIlJun <tack89826@gmail.com>
2023-09-14 16:23:11 +0900

    ProductReview

---
+++ src/main/java/tack/project/boot02/entity/ProductReview.java
@@ -0,0 +1,40 @@
+package tack.project.boot02.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "product")
+public class ProductReview {
+
+    ////////////////////////////////////////////////////
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    private String content;
+
+    private String reviewer;
+
+    private int score;
+
+    ////////////////////////////////////////////////////
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+    // 자바코드는 논리적으로 생각해라.
+    
+}
