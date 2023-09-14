73a820ef52b2edeaa9cb5f0e1bd97c2ed5e49abc
tarkIlJun <tack89826@gmail.com>
2023-09-14 18:52:43 +0900

    ProductRepository

--- src/main/java/tack/project/boot02/repository/ProductRepository.java
+++ src/main/java/tack/project/boot02/repository/ProductRepository.java
@@ -1,5 +1,19 @@
 package tack.project.boot02.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import tack.project.boot02.entity.Product;
+import tack.project.boot02.repository.search.ProductSearch;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
+
+    //////////////////////////////////////////////////////////////////////////
+    // 상세보기 JPQL.
+    @EntityGraph(attributePaths = "images")
+    @Query("select p from Product p where p.delFlag = false and p.pno = :pno")
+    Product selectOne(@Param("pno") Long pno);
     
 }
