ff795c0826a5bcd508962787d29252148c683930
tarkIlJun <tack89826@gmail.com>
2023-09-14 18:56:43 +0900

    ProductService

---
+++ src/main/java/tack/project/boot02/service/ProductService.java
@@ -0,0 +1,24 @@
+package tack.project.boot02.service;
+
+import jakarta.transaction.Transactional;
+import tack.project.boot02.dto.PageRequestDTO;
+import tack.project.boot02.dto.PageResponseDTO;
+import tack.project.boot02.dto.ProductDTO;
+import tack.project.boot02.dto.ProductListDTO;
+
+@Transactional
+public interface ProductService {
+
+    /////////////////////////////////////////////////////////////////
+    PageResponseDTO<ProductListDTO> list(PageRequestDTO requestDTO);
+
+    /////////////////////////////////////////////////////////////////
+    Long register(ProductDTO productDTO);
+
+    ProductDTO readOne(Long pno);
+
+    void remove(Long pno);
+
+    void modify(ProductDTO productDTO);
+    
+}
