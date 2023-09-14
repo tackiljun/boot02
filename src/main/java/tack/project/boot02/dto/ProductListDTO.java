1f9380650e32db0c5f4b6eb8a62655e2b9835258
tarkIlJun <tack89826@gmail.com>
2023-09-14 15:11:43 +0900

    Product-DTO

---
+++ src/main/java/tack/project/boot02/dto/ProductListDTO.java
@@ -0,0 +1,22 @@
+package tack.project.boot02.dto;
+
+import lombok.Data;
+
+
+@Data
+public class ProductListDTO {
+
+    // 목록 가져오는 DTO.
+    //////////////////////////
+    private Long pno;
+    private String pname;
+    private int price;
+
+    //////////////////////////
+    private String fname;
+    
+    //////////////////////////
+    private long reviewCnt;
+    private double reviewAvg;
+    
+}
