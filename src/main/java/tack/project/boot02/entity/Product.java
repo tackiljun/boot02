1b6aaddf410a73bd4285d33fc64be51af00f4863
tarkIlJun <tack89826@gmail.com>
2023-09-14 15:17:17 +0900

    Product

---
+++ src/main/java/tack/project/boot02/entity/Product.java
@@ -0,0 +1,18 @@
+package tack.project.boot02.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "images")
+public class Product {
+    
+}
