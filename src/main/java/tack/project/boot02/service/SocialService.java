834d3e15681e55703fc7fa5926fa224f1c31dbda
tarkIlJun <tack89826@gmail.com>
2023-09-15 20:27:09 +0900

    SocialService

---
+++ src/main/java/tack/project/boot02/service/SocialService.java
@@ -0,0 +1,11 @@
+package tack.project.boot02.service;
+
+import jakarta.transaction.Transactional;
+
+
+@Transactional
+public interface SocialService {
+
+    String getKakaoEmail(String authCode);
+    
+}
