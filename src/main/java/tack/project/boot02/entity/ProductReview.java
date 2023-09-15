8b0213ea647b7b8dba6309436d460b8ea26485d0
tarkIlJun <tack89826@gmail.com>
2023-09-15 19:02:13 +0900

    ProductReview

--- src/main/java/tack/project/boot02/entity/ProductReview.java
+++ src/main/java/tack/project/boot02/entity/ProductReview.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
