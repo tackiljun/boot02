d4edc6ea02e6e2a1c2fc5fec584f69bef337e8a7
tarkIlJun <tack89826@gmail.com>
2023-09-15 19:01:08 +0900

    Member

--- src/main/java/tack/project/boot02/entity/Member.java
+++ src/main/java/tack/project/boot02/entity/Member.java
@@ -2,6 +2,7 @@ package tack.project.boot02.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
