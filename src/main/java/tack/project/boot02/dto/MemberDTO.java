fd58db67b6f0d2caee0ce23fad3da06ea6a3e9e7
tarkIlJun <tack89826@gmail.com>
2023-09-15 18:53:11 +0900

    MemberDTO

--- src/main/java/tack/project/boot02/dto/MemberDTO.java
+++ src/main/java/tack/project/boot02/dto/MemberDTO.java
@@ -1,5 +1,32 @@
 package tack.project.boot02.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
+
+    ///////////////////////////////////////////////////////
+    private String email;
+    ///////////////////////////////////////////////////////
+    //@JsonIgnore
+    private String pw;
+    ///////////////////////////////////////////////////////
+    private String nickname;
+    ///////////////////////////////////////////////////////
+    private boolean admin;
+    // 이게 JSON데이터로 바뀔꺼야.
+    // 지난주에 만들었던 이니셜스테이트의 내용이 얘네가 될거야.
+    // 쿠키로 저장할꺼야.
+    // jsonformat / jsonignore.
+    ///////////////////////////////////////////////////////
+    private String accessToken;
+    ///////////////////////////////////////////////////////
+    private String refreshToken;
     
 }
