7f56b55850c5c0043161e2266d2bfcb2ec69a665
tarkIlJun <tack89826@gmail.com>
2023-09-15 18:56:44 +0900

    Member

---
+++ src/main/java/tack/project/boot02/entity/Member.java
@@ -0,0 +1,27 @@
+package tack.project.boot02.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    ///////////////////////////////////
+    @Id
+    private String email; // 아이디역할.
+    private String pw; // 패스워드.
+    private String nickname;
+    private boolean admin;
+    
+}
