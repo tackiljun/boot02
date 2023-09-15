21951a66d469ad689cf04cfee9e7ca7616c600fa
tarkIlJun <tack89826@gmail.com>
2023-09-15 19:00:34 +0900

    MemberService

---
+++ src/main/java/tack/project/boot02/service/MemberService.java
@@ -0,0 +1,16 @@
+package tack.project.boot02.service;
+
+import jakarta.transaction.Transactional;
+import tack.project.boot02.dto.MemberDTO;
+
+
+@Transactional
+public interface MemberService {
+
+    ///////////////////////////////////////////
+    MemberDTO login(String email, String pw);
+
+    ///////////////////////////////////////////
+    MemberDTO getMemberWithEmail(String email);
+    
+}
