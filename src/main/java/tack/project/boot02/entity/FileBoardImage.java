8315c8aa89eda7bc58423d24d7a7a53e29b5a67e
tarkIlJun <tack89826@gmail.com>
2023-09-16 11:59:44 +0900

    FileBoardImage

---
+++ src/main/java/tack/project/boot02/entity/FileBoardImage.java
@@ -0,0 +1,37 @@
+package tack.project.boot02.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileBoardImage {
+
+    ///////////////////////////////////////////////////
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imgno;
+
+    private String uuid;
+
+    private String fname;
+
+    private int ord;
+
+    public void changeOrd (int ord) {
+        this.ord = ord;
+    }
+    // 게시판에서 이미지 순번정하는 메소드.
+    
+}
