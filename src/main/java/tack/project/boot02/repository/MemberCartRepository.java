7642260df3cb84375c56c90bd0cdd52f02b7846b
tarkIlJun <tack89826@gmail.com>
2023-09-16 12:48:39 +0900

    MemberCartRepository

---
+++ src/main/java/tack/project/boot02/repository/MemberCartRepository.java
@@ -0,0 +1,18 @@
+package tack.project.boot02.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import tack.project.boot02.entity.MemberCart;
+
+
+public interface MemberCartRepository extends JpaRepository<MemberCart, Long> {
+
+    //////////////////////////////////////////////////////////////////////////////////
+    @Query("select mc from MemberCart mc where mc.email = :email order by mc.cno asc")
+    List<MemberCart> selectCart(@Param("email") String email);
+    
+}
