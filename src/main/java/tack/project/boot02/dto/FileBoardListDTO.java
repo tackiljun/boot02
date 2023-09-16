42344dd530eebff945406bcbc6539040efb04d9e
tarkIlJun <tack89826@gmail.com>
2023-09-16 11:48:19 +0900

    FileBoardListDTO

---
+++ src/main/java/tack/project/boot02/dto/FileBoardListDTO.java
@@ -0,0 +1,26 @@
+package tack.project.boot02.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileBoardListDTO {
+
+    /////////////////////
+    // 게시판 번호.
+    private Long bno;
+    //게시판 제목.
+    private String title;
+    // 이미지의 uuid.
+    private String uuid;
+    // 이미지의 파일이름.
+    private String fname;
+    
+}
